182394b3a3361f4eff3ae4c07886608785e1db63
1250523689@qq.com <1250523689@qq.com>
2023-09-21 10:23:05 -0500

    ADD CART

--- addcart.php
+++ addcart.php
@@ -1,18 +1,33 @@
 <?php
     //notice : you don't need to login to add to cart 
     $page_title="added to cart";
-    require("header.php");
-    require("footer.php");
+    $self=basename($_SERVER['PHP_SELF']);
+    require("./inc/header.php");
+    require("./inc/footer.php");
     require("./inc/dbconnect.php");
     
 
     echo "<div class='w3-container w3-teal'><h1>".$page_title."
 <a href='viewcart.php'><i class='fas fa-shopping-cart' id='caticon' 
 title='View Cart'></i></a></h1></div> 
-<div= 'container'><p class='indent'>You have added , 
+
 ";
-$prod_id=$_GET['prod_id'];
-echo "the product id is $prod_id, yeah it worked";
+
+
+//check for one valid produc id
+if(isset($_GET['prod_id']) && filter_var($_GET['prod_id'],FILTER_VALIDATE_INT,
+array('min_range'=>1))){
+    $prod_id=$_GET['prod_id'];
+    // get the product id 
+    //$prod_id=$_GET['prod_id'];
+    echo "the product id is $prod_id, yeah it worked";
+}
+else{
+    echo '<div class="w3-container w3-red"><h1>Oh No!</h1>
+    <p class="indent"> THis page has been accessed in error.</p></div>
+    ';
+}
+
 
 
 
